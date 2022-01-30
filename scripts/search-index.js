d60a983a6158ec8e3ddf3d8d9aa383b3dc75ac64
beagle42 <42beagles42@gmail.com>
2022-01-30 16:35:22 +1000

    Add files via upload

--- scripts/search-index.js
+++ scripts/search-index.js
@@ -8,11 +8,12 @@ var Page = function (name, blurb, url) {
 //index
 var index = [
 	new Page("Home", "The home page", "index.html"),
-	new Page("Beagle Search", "Search this website", "beagle.html"),
+	new Page("Beagle Search", "Search for the pages of this website", "beagle.html"),
 	new Page("Calendrical Conversions", "Convert between different calendars", "calendrical-conversions.html"),
 	new Page("Dials", "An astronomical clock", "dials.html"),
 	new Page("Astronomical Clock", "That's what it is", "astronomical-clock.html"),
-	new Page("Equation of Time", "Several ways of representing its current value", "equation-of-time.html"),
+	new Page("Equation of Time", "Its current value", "equation-of-time.html"),
 	new Page("Horoscope Generator", "Generates a horoscope chart for any time, date and location", "horoscope.html"),
-	new Page("English Numbers", "eg. It converts '42' to 'forty-two'. You get the idea.", "english-numbers.html")
+	new Page("English Numbers", "eg. It converts '42' to 'forty-two'. You get the idea.", "english-numbers.html"),
+	new Page("Zodiacal Clock", "An astronomical clock", "zodiacal-clock.html")
 ];
